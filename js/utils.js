4d994ae4d515432e224aeb9a642d6d1fa9208b2e
Arpan Shah <arpanshah102@gmail.com>
2026-05-07 22:43:23 -0400

    Add more files and set up file structure and name

--- js/utils.js
+++ js/utils.js
@@ -5,7 +5,35 @@ function setMoney(amount) {
     localStorage.setItem('money', amount);
 }
 function calcMoney(amount, symbol = '+') {
-    let currentMoney = getMoney();
+    calcCurrency(amount, 'm', symbol);
+}
+
+function getCrypto() {
+    return parseFloat(localStorage.getItem('crypto')) || 0;
+}
+function setCrypto(amount) {
+    localStorage.setItem('crypto', amount);
+}
+function calcCrypto(amount, symbol = '+') {
+    calcCurrency(amount, 'c', symbol);
+}
+
+function getStardust() {
+    return parseFloat(localStorage.getItem('stardust')) || 0;
+}
+function setStardust(amount) {
+    localStorage.setItem('stardust', amount);
+}
+function calcStardust(amount, symbol = '+') {
+    calcCurrency(amount, 's', symbol);
+}
+
+function calcCurrency(amount, currency, symbol) {
+    let currentMoney;
+    if (currency === 'm') {currentMoney = getMoney();}
+    else if (currency === 'c') {currentMoney = getCrypto();}
+    else if (currency === 's') {currentMoney = getStardust();}
+
     if (symbol === '+') {
         currentMoney += amount;
     } else if (symbol === '-') {
@@ -19,10 +47,19 @@ function calcMoney(amount, symbol = '+') {
     } else if (symbol === '**') {
         currentMoney **= amount;
     }
-    setMoney(currentMoney);
+
+    if (currency === 'm') {setMoney(currentMoney);}
+    else if (currency === 'c') {setCrypto(currentMoney);}
+    else if (currency === 's') {setStardust(currentMoney);}
 }
-function formatMoney(amount) {
-    return amount.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+function formatMoney(amount, type = 'm') {
+    if (type === 'm') {
+        return amount.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    } else if (type === 'c') {
+        return '₡' + amount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    } else if (type === 's') {
+        return '✺' + Math.floor(amount).toLocaleString();
+    }
 }
 
 function getAchievementsUnlocked() {
@@ -41,7 +78,7 @@ function isAchievementUnlocked(id) {
 }
 
 function getSlotsStats() {
-    return JSON.parse(localStorage.getItem('slotsStats')) || { spins: 0, jackpots: 0, nearMisses: 0, upgradeBonus: { rigMachine: 0, payout: [20, 400], cashback: 0, turbo: 100 }, dripBonus: 0, themeBonus: 0 };
+    return JSON.parse(localStorage.getItem('slotsStats')) || { spins: 0, jackpots: 0, nearMisses: 0, upgradeBonus: { rigMachine: 0, payout: [20, 400], cashback: 0, pity: 100 }, dripBonus: 0, themeBonus: 0 };
 }
 
 function getTheme() {
@@ -71,9 +108,9 @@ function isCryptoGamemodeUnlocked() {
 function unlockCryptoGamemode() {
     localStorage.setItem('cryptoGamemodeUnlocked', true);
 }
-function isTheWorldUnlocked() {
-    return JSON.parse(localStorage.getItem('theWorldUnlocked')) || false;
+function isMadeInHeavenUnlocked() {
+    return JSON.parse(localStorage.getItem('madeInHeavenUnlocked')) || false;
 }
-function unlockTheWorld() {
-    localStorage.setItem('theWorldUnlocked', true);
+function unlockMadeInHeaven() {
+    localStorage.setItem('madeInHeavenUnlocked', true);
 }
